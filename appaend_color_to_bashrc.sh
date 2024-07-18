77b4c58c35d4d4c084033405d527ce1a6eac885a
Rei Halle <reihalle@gmail.com>
2024-07-18 10:20:51 +0300

    Add color apperance to prompt shell

---
+++ appaend_color_to_bashrc.sh
@@ -0,0 +1,15 @@
+FILENAME=${HOME}/.bashrc
+if [ -f $FILENAME ]; then
+	if ! grep -Fq 'color generate by script' $FILENAME; then
+		echo "## color generate by script ##">> $FILENAME
+                echo '_RED=$(tput setaf 1)'>> $FILENAME
+		echo '_GREEN=$(tput setaf 2)'>> $FILENAME 
+                echo '_YELLOW=$(tput setaf 3)'>> $FILENAME
+                echo '_BLUE=$(tput setaf 4)'>> $FILENAME
+                echo '_MAGENTA=$(tput setaf 5)'>> $FILENAME
+                echo '_CYAN=$(tput setaf 6)'>> $FILENAME
+                echo '_RESET=$(tput sgr0)'>> $FILENAME
+                echo '_BOLD=$(tput bold)'>> $FILENAME
+                echo 'export PS1="[${_RED}\u${_CYAN}@${_BLUE}\h${_MAGENTA} \W${_RESET}]${_BOLD}#${_RESET}"'>> $FILENAME
+	fi
+fi
